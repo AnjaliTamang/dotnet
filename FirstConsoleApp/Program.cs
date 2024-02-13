8cfea33bb5a03dbf5f71e107e7333f3c0ff7766c
Anjali Tamang <lopchananjali@gmail.com>
2024-02-13 07:38:16 +0545

    first commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//Numbers
+byte age=255;
+byte dayOfWeek=11;
+short b=23434;
+int c=2422435;
+long d=32423235;
+float weight1=45.6f;
+double weight2=45678.6;
+decimal weight3=435678.86982646562m;
+//Text
+char initial='D';
+string name="sdlkf";
+bool? isFemale = null;
+DateTime date= DateTime.Now;
+Console.WriteLine(date);
+
